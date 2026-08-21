2c224efcb93a0e8003d4e215680da3d2fe4a63b7
TheEagle-Benson <theeaglebenson@gmail.com>
2026-08-21 07:00:34 +0000

    feat.: add user freindly error

--- js/record.js
+++ js/record.js
@@ -1,3 +1,4 @@
+let user_error_str
 let geo_optional_args = {
   enableHighAccuracy: true,
   timeout: 120000,
@@ -13,7 +14,25 @@ function success_callback(position) {
 }
 
 function error_callback(error) {
-  console.error(error)
+  switch (error.code) {
+    case 1:
+      console.warn("User denied location access")
+      user_error_str = "You denied location access. Kindly allow location for this site."
+      break;
+      case 2:
+        console.warn("location information is not available")
+        user_error_str = "Could not get your location information."
+        break;
+      case 3:
+        console.warn("Request timeout")
+        user_error_str = `Request timeout. Could not resolve your location within ${geo_optional_args.timeout / 60000} minutes`
+        console.log(user_error_str)
+        break;
+      
+    default:
+      console.error("An unknown error occured", error.message)
+      user_error_str = "An unknown error occurred"
+  }
 }
 
 function get_location() {
