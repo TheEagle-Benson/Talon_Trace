85d4c52d56ff575d4853fd7d57d642bf37526deb
TheEagle-Benson <theeaglebenson@gmail.com>
2026-09-13 10:59:15 +0000

    update: add unique like constraint to name field

--- js/db.js
+++ js/db.js
@@ -1,7 +1,7 @@
 const db = new Dexie('Talon_Trace_db')
 
 db.version(1).stores({
-  trail: '++id, name, created_at'
+  trail: '++id, &name, created_at'
 })
 
 export async function saveTrail(trailObj) {
